225239b8ccdda91e2c60fb8cd9e495fa1bc01108
RNKbansal23 <rnkbansal123@gmail.com>
2025-12-14 22:55:53 +0530

    feat: Add Prometheus instrumentation to Flask app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrices
 
 app = Flask(__name__)
+metrices = PrometheusMetrices(app)
 
 def fibonacci(n):
     if n <= 0:
